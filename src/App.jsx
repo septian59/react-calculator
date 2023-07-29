001e8ac04d950f150e1137e63914c80e793ed784
unknown <septianpriman59@gmail.com>
2023-07-29 23:07:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Screen from "./components/Screen";
+import "./index.css";
+import Wrapper from "./components/Wrapper";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+import CalcProvider from "./context/CalcContext";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+function App() {
+  return (
+    <>
+      <div>
+        <CalcProvider>
+          <Wrapper>
+            <Screen />
+            <ButtonBox>
+              {btnValues.flat().map((btn, i) => (
+                <Button value={btn} key={i} />
+              ))}
+            </ButtonBox>
+          </Wrapper>
+        </CalcProvider>
+      </div>
+    </>
+  );
+}
+
+export default App;
